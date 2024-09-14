9d6c806665d2136046a6608a56a0f375312a0135
aditya-barik <aditya.barik@outlook.com>
2024-09-14 14:59:42 +0530

    added environment requirements ans setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cd /d/learn-ab/learning-PySpark
+
+source .env/Scripts/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+echo -e "\033[92mEnvironment setup successful. Enjoy Coding!!!\033[0m"
