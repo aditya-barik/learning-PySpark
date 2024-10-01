76f41b226fcdc9aa8dbbf7ae59531d0f72c2a501
aditya-barik <aditya.barik@outlook.com>
2024-10-01 14:43:45 +0530

    updated setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-cd /d/learn-ab/learning-PySpark
-
-source .env/Scripts/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-echo -e "\033[92mEnvironment setup successful. Enjoy Coding!!!\033[0m"
+echo -e "\033[92mEnvironment setup successful. Enjoy learning PySpark!!!\033[0m"
